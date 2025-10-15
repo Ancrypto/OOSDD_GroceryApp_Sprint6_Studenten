5f8e072ba72b89a7d5c348e9182026b04bf2291c
Ancrypto <marnix.rischke@gmail.com>
2025-10-15 17:06:36 +0200

    NewProductView en model toegevoegd

---
+++ Grocery.App/ViewModels/NewProductViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+using Grocery.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.App.ViewModels
+{
+    public partial class NewProductViewModel : BaseViewModel
+    {
+        private readonly IProductService _productService;
+
+        Client client;
+        public NewProductViewModel(IProductService productService, GlobalViewModel global)
+        {
+            _productService = productService;
+            client = global.Client;
+        }
+
+        [RelayCommand]
+        private void AddProduct(Product product)
+        {
+            if(client.Role == Role.Admin)
+            {
+                _productService.Add(product);
+            }
+        }
+    }
+}
